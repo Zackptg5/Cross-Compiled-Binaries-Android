bf069047d17cd7c58aca5d5c6ca58c3c768e218a
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-03-14 14:30:05 -0400

    bug fix, sdcard takes longer to mount sometimes
    fix

--- busybox/install.sh
+++ busybox/install.sh
@@ -1,6 +1,6 @@
 cd $MODDIR$insloc
 for j in $(./$i --list); do
-  if [ [ ! -e "/sbin/.magisk/mirror$(echo $insloc | sed 's|^/system/vendor|/vendor|')/$j" ] && [ ! -e "$j" ]; then
+  if [ ! -e "/sbin/.magisk/mirror$(echo $insloc | sed 's|^/system/vendor|/vendor|')/$j" ] && [ ! -e "$j" ]; then
     ln -sf $i $j
     echo "/$(echo $insloc | cut -d / -f3-)" >> $MODDIR/.$ibinary
   fi
--- ccbins_files/service.sh
+++ ccbins_files/service.sh
@@ -1,17 +1,17 @@
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 MODPATH=${0%/*}
-filever=2
+filever=3
 (
-until [ "$(getprop sys.boot_completed)" == "1" ]; do
-  sleep 5
+until [ "$(getprop sys.boot_completed)" == "1" ] && [ -d /storage/emulated/0/Android ]; do
+  sleep 1
 done
 [ -f /storage/emulated/0/.aliases ] || touch /storage/emulated/0/.aliases
 [ "$(grep 'cu()' /storage/emulated/0/.aliases 2>/dev/null)" ] || echo -e 'cu() {\n  coreutils --coreutils-prog=${@}\n}' >> /storage/emulated/0/.aliases
 [ "$(grep 'TERMINFO=' /storage/emulated/0/.aliases 2>/dev/null)" ] || echo 'export TERMINFO=/system/usr/share/terminfo' >> /storage/emulated/0/.aliases
 if [ -f $MODPATH/system/etc/zsh/.zshrc ] && [ ! -f /storage/emulated/0/.zsh/.zshrc ]; then
-    mkdir -p /storage/emulated/0/.zsh 2>/dev/null
-    cp -f $MODPATH/system/etc/zsh/.zshrc /storage/emulated/0/.zsh/.zshrc
-    [ -d /storage/emulated/0/.zsh/custom ] || cp -rf $MODPATH/system/etc/zsh/.oh-my-zsh/custom /storage/emulated/0/.zsh/
-  done
+  mkdir -p /storage/emulated/0/.zsh 2>/dev/null
+  cp -f $MODPATH/system/etc/zsh/.zshrc /storage/emulated/0/.zsh/.zshrc
+  [ -d /storage/emulated/0/.zsh/custom ] || cp -rf $MODPATH/system/etc/zsh/.oh-my-zsh/custom /storage/emulated/0/.zsh/
+fi
 )&
