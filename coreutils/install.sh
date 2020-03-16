aeb7c6f18258681c614ab587b33c1e6de92ad00a
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-03-16 17:03:27 -0400

    fix box install/uninstall

--- coreutils/install.sh
+++ coreutils/install.sh
@@ -2,7 +2,7 @@ cd $MODDIR$insloc
 for j in $(./$i --help | sed -n "/^ \[/p"); do
   if ([ ! -e "/sbin/.magisk/mirror$(echo $insloc | sed 's|^/system/vendor|/vendor|')/$j" ] && [ ! -e "$j" ] && [ "$j" != "[" ] && [ "$j" != "test" ]) || ([ "$j" == "cp" ] || [ "$j" == "mv" ]); then
     ln -sf $i $j
-    echo "/$(echo $insloc | cut -d / -f3-)" >> $MODDIR/.$ibinary
+    echo "$(echo $insloc | sed "s|$MODDIR||")/$j" >> $MODDIR/.$ibinary
   fi
 done
 cd $dir
