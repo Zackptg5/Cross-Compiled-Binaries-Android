d58d95dfaf932253f2a240a19383f9190a7e19f9
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2021-05-26 14:34:16 -0400

    Update curl, fix cp/mv alias

--- ccbins_files/service.sh
+++ ccbins_files/service.sh
@@ -1,14 +1,18 @@
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 MODPATH=${0%/*}
-filever=6
+filever=7
 (
 until [ "$(getprop sys.boot_completed)" == "1" ] && [ -d /storage/emulated/0/Android ]; do
   sleep 1
 done
 [ -f /storage/emulated/0/.aliases ] || touch /storage/emulated/0/.aliases
-grep -q 'alias cp=' /storage/emulated/0/.aliases 2>/dev/null || echo "alias cp='cp -g'" >> /storage/emulated/0/.aliases
-grep -q 'alias mv=' /storage/emulated/0/.aliases 2>/dev/null || echo "alias mv='mv -g'" >> /storage/emulated/0/.aliases
+if [ "$(grep 'Coreutils' $MODPATH/.installed)" ]; then
+  grep -q 'alias cp=' /storage/emulated/0/.aliases 2>/dev/null || echo "alias cp='cp -g'" >> /storage/emulated/0/.aliases
+  grep -q 'alias mv=' /storage/emulated/0/.aliases 2>/dev/null || echo "alias mv='mv -g'" >> /storage/emulated/0/.aliases
+else
+  sed -i -e '/cp -g/d' -e '/mv -g/d' /storage/emulated/0/.aliases
+fi
 grep -q 'cu()' /storage/emulated/0/.aliases 2>/dev/null || echo -e 'cu() {\n  coreutils --coreutils-prog=${@}\n}' >> /storage/emulated/0/.aliases
 grep -q 'TERMINFO=' /storage/emulated/0/.aliases 2>/dev/null || echo 'export TERMINFO=/system/usr/share/terminfo' >> /storage/emulated/0/.aliases
 
