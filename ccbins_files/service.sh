bd70428e8d8419f132359a2b994e07b56f88e475
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2022-08-09 10:38:48 -0400

    bug fix

--- ccbins_files/service.sh
+++ ccbins_files/service.sh
@@ -10,11 +10,11 @@ done
 grep -q 'TERMINFO=' /storage/emulated/0/.aliases 2>/dev/null || echo 'export TERMINFO=/system/usr/share/terminfo' >> /storage/emulated/0/.aliases
 
 if [ "$(grep 'Coreutils' $MODPATH/.installed)" ]; then
-  grep -q 'cu()' /storage/emulated/0/.aliases 2>/dev/null || echo -e 'cu() {\n  coreutils --coreutils-prog=${@}\n}#cu' >> /storage/emulated/0/.aliases
+  grep -q 'cu()' /storage/emulated/0/.aliases 2>/dev/null || echo -e 'cu() {\n  coreutils --coreutils-prog=${@}\n} #cu' >> /storage/emulated/0/.aliases
   grep -q 'alias cp=' /storage/emulated/0/.aliases 2>/dev/null || echo "alias cp='cp -g'" >> /storage/emulated/0/.aliases
   grep -q 'alias mv=' /storage/emulated/0/.aliases 2>/dev/null || echo "alias mv='mv -g'" >> /storage/emulated/0/.aliases
 else
-  sed -i -e '/cp -g/d' -e '/mv -g/d' -e '/cu() {/,/}#cu/d' /storage/emulated/0/.aliases
+  sed -i -e '/cp -g/d' -e '/mv -g/d' -e '/cu() {/,/} #cu/d' /storage/emulated/0/.aliases
 fi
 if [ "$(grep 'Findutils' $MODPATH/.installed)" ]; then # Needs to be set to new mirror path each boot
   sed -i '/updatedb/d' /storage/emulated/0/.aliases
