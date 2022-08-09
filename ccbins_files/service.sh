a4538d54d32dc3141fd7769ea878d134cca74fd0
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2022-08-08 23:05:20 -0400

    update vim, fix bug #74
    Fix coreutils alias remove bug

--- ccbins_files/service.sh
+++ ccbins_files/service.sh
@@ -1,7 +1,7 @@
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 MODPATH=${0%/*}
-filever=8
+filever=9
 (
 until [ "$(getprop sys.boot_completed)" == "1" ] && [ -d /storage/emulated/0/Android ]; do
   sleep 1
@@ -10,17 +10,23 @@ done
 grep -q 'TERMINFO=' /storage/emulated/0/.aliases 2>/dev/null || echo 'export TERMINFO=/system/usr/share/terminfo' >> /storage/emulated/0/.aliases
 
 if [ "$(grep 'Coreutils' $MODPATH/.installed)" ]; then
-  grep -q 'cu()' /storage/emulated/0/.aliases 2>/dev/null || echo -e 'cu() {\n  coreutils --coreutils-prog=${@}\n}' >> /storage/emulated/0/.aliases
+  grep -q 'cu()' /storage/emulated/0/.aliases 2>/dev/null || echo -e 'cu() {\n  coreutils --coreutils-prog=${@}\n}#cu' >> /storage/emulated/0/.aliases
   grep -q 'alias cp=' /storage/emulated/0/.aliases 2>/dev/null || echo "alias cp='cp -g'" >> /storage/emulated/0/.aliases
   grep -q 'alias mv=' /storage/emulated/0/.aliases 2>/dev/null || echo "alias mv='mv -g'" >> /storage/emulated/0/.aliases
 else
-  sed -i -e '/cp -g/d' -e '/mv -g/d' -e '/cu()/d' /storage/emulated/0/.aliases
+  sed -i -e '/cp -g/d' -e '/mv -g/d' -e '/cu() {/,/}#cu/d' /storage/emulated/0/.aliases
 fi
 if [ "$(grep 'Findutils' $MODPATH/.installed)" ]; then # Needs to be set to new mirror path each boot
   sed -i '/updatedb/d' /storage/emulated/0/.aliases
   echo "alias updatedb='updatedb --prunepaths=\"/proc $(magisk --path)/.magisk/mirror\"'" >> /storage/emulated/0/.aliases
 else
   sed -i '/updatedb/d' /storage/emulated/0/.aliases
+fi
+if [ "$(grep 'Vim' $MODPATH/.installed)" ]; then
+  grep -q 'export VIM=' /storage/emulated/0/.aliases 2>/dev/null || echo "export VIM=/system/usr/share/vim" >> /storage/emulated/0/.aliases
+  grep -q 'export VIMRUNTIME=' /storage/emulated/0/.aliases 2>/dev/null || echo "export VIMRUNTIME=/system/usr/share/vim/vim90" >> /storage/emulated/0/.aliases
+else
+  sed -i -e '/VIM=/d' -e '/VIMRUNTIME=/d' /storage/emulated/0/.aliases
 fi
 
 if [ -f $MODPATH/system/etc/zsh/.zshrc ] && [ ! -f /storage/emulated/0/.zsh/.zshrc ]; then
