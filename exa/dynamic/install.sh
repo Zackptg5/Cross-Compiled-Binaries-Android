4e7c3984cfbee5568f7c06ddc343eed61991999f
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2024-03-30 16:41:48 -0400

    fix bootloop

--- exa/dynamic/install.sh
+++ exa/dynamic/install.sh
@@ -2,4 +2,4 @@
 # Since same libs are shared between multiple binaries, don't remove libs on uninstall of binary
 rm -f $MODDIR/.$ibinary
 # Don't replace existing libz - leads to bootloop
-[ -f "$(find /dev -mindepth 2 -maxdepth 2 -type d -name ".magisk")/mirror/system/lib/libz.so" ] && rm -f $MODDIR/system/lib*/libz.so
+[ -f "/system/lib/libz.so" ] && rm -f $MODDIR/system/lib*/libz.so
